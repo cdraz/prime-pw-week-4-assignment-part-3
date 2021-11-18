a90f0396463098aacd462708cd111e7cb4c30a4a
Christopher Razidlo <cd.razidlo@gmail.com>
2021-11-18 15:05:54 -0600

    completing week 4 part 3 assignment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,27 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// Declaring empty basket variable
+let basket = []
+
+// declaring addItem function
+function addItem(item) {
+    basket.push(item);
+    return true;
+} // end addItem
+
+// testing addItem function
+
+addItem("Hoodie");
+addItem("Sneakers");
+addItem("Fanny pack");
+
+
+// Declaring listItems function
+function listItems() {
+    for (item of basket) {
+        console.log(item);
+    }
+} // end listItems
+
